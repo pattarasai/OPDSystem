7cc1854658a3b95755046183eb3cdd3dcf53bcc6
pattarasai markpeng <pattarasai.m@hotmail.com>
2013-08-11 23:42:43 +0700

    uploadfile

---
+++ MvcApplication4/MvcApplication4/Controllers/loginController.cs
@@ -0,0 +1,41 @@
+﻿using MvcApplication4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcApplication4.Controllers
+{
+    public class loginController : Controller
+    {
+        //
+        // GET: /login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.Username == "ben" && model.Password == "ben")
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, true);
+                    //if true, user are also in logged in state,although they close their web.
+                    return RedirectToAction("Index", "Profile");
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username and password");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ MvcApplication4/MvcApplication4/Controllers/profileController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication4.Controllers
+{
+    public class profileController : Controller
+    {
+        //
+        // GET: /profile/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
